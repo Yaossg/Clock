819a191c4892828cbd4abdca0e34c2e2f39b1c4f
Yaossg <yaossg@outlook.com>
2022-11-02 09:59:54 +0800

    task 2

--- app/src/main/java/com/yaossg/clock/MainActivity.kt
+++ app/src/main/java/com/yaossg/clock/MainActivity.kt
@@ -2,6 +2,8 @@ package com.yaossg.clock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import android.widget.TextView
 import java.text.SimpleDateFormat
@@ -10,6 +12,9 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
     lateinit var clockTextView: TextView
     lateinit var updateButton: Button
+
+    val handler = Handler(Looper.getMainLooper())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,6 +25,13 @@ class MainActivity : AppCompatActivity() {
         updateButton.setOnClickListener {
             clockTextView.text = SimpleDateFormat("hh:mm:ss", Locale.US).format(Date())
         }
-        updateButton.callOnClick()
+
+        handler.post(object : Runnable {
+            override fun run() {
+                updateButton.callOnClick()
+                handler.postDelayed(this, 1000)
+            }
+        })
+
     }
 }

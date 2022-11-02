37f26882cfe44a2183407c755ba623571f1c53ac
Yaossg <yaossg@outlook.com>
2022-11-02 09:57:31 +0800

    task 1

--- app/src/main/java/com/yaossg/clock/MainActivity.kt
+++ app/src/main/java/com/yaossg/clock/MainActivity.kt
@@ -2,10 +2,24 @@ package com.yaossg.clock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var clockTextView: TextView
+    lateinit var updateButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        clockTextView = findViewById(R.id.clockTextView)
+        updateButton = findViewById(R.id.updateButton)
+
+        updateButton.setOnClickListener {
+            clockTextView.text = SimpleDateFormat("hh:mm:ss", Locale.US).format(Date())
+        }
+        updateButton.callOnClick()
     }
 }
